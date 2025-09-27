47448a2bee31bc56a7e6caba9213a4c3e7247b63
Kaleb Tomanovich <kalebtomanovich@gmail.com>
2025-09-27 10:24:17 -0400

    Updated

--- Assignments/Assignment_3/index.php
+++ Assignments/Assignment_3/index.php
@@ -14,7 +14,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>add names</title>
+    <title>add names Project</title>
   </head>
   <body>
     <div class="container">
